64f7f0f844f6bdffc51694e115cc86719447e40c
DanBarredo <dvbarredo@gmail.com>
2020-08-28 13:09:10 +0100

    edited searchLyrics() function

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const artist = document.getElementById("artist");
 const title = document.getElementById("title");
 const lyrics = document.querySelector(".lyrics");
 
-
-var searchQuery = {artist:artist.value, title: title.value}
+var searchQuery = {
+    artist: artist.value,
+    title: title.value
+}
 
 // *** EVENT LISTENERS ***
 // load page
@@ -14,7 +16,10 @@ window.addEventListener("DOMContentLoaded", resetSearch);
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    var searchQuery = {artist:artist.value, title: title.value}
+    var searchQuery = {
+        artist: artist.value,
+        title: title.value
+    }
     if (!searchQuery.artist && !searchQuery.title) {
         console.log("both inputs empty");
         alertMessage("please enter an artist and song title", "fail");
@@ -26,21 +31,40 @@ form.addEventListener("submit", function(e) {
     if (!searchQuery.title) {
         console.log("title search empty");
         alertMessage("please enter a song title", "fail");
-    }
-    else {
-        alertMessage("found some Lyrics", "success");
+    } else {
+        // alertMessage("found some Lyrics", "success");
         searchLyrics(searchQuery);
     }
 });
 
-
 // *** FUNCTIONS ***
 
 function searchLyrics(searchQuery) {
-    $.get("https://api.lyrics.ovh/v1/" + searchQuery.artist + "/" + searchQuery.title,
-        function(data) {
-            lyrics.innerHTML = getSongDetails() + data.lyrics.replace(new RegExp("\n", "g"),"<br>");
-        });
+    var url = buildURL(searchQuery.artist, searchQuery.title);
+    var request = new XMLHttpRequest();
+
+    request.open('GET', url);
+    request.onreadystatechange = function() {
+        if (this.readyState === 4) {
+            console.log('Status:', this.status);
+            console.log('Headers:', this.getAllResponseHeaders());
+            // console.log('Body:', this.responseText);
+            if (request.status == 404) {
+                console.log("error, nothing found");
+                alertMessage("Couldn't find any lyrics! try again", "fail");
+            } else {
+                alertMessage("found some Lyrics", "success");
+                $.get(url, function(data) {
+                    lyrics.innerHTML = getSongDetails() + data.lyrics.replace(new RegExp("\n", "g"), "<br>");
+                });
+            }
+        }
+    };
+    request.send();
+}
+
+function buildURL(artist, title) {
+    return ('https://api.lyrics.ovh/v1/' + artist + '/' + title);
 }
 
 function getSongDetails() {
@@ -52,7 +76,6 @@ function resetSearch() {
     title.value = "";
 }
 
-
 function alertMessage(msg, status) {
     alert = form.querySelector(".alert");
     alert.classList.add(`alert-${status}`)
@@ -63,5 +86,5 @@ function alertMessage(msg, status) {
         alert.textContent = "";
         alert.classList.remove(`alert-${status}`);
         // alert.style.height = 0;
-    }, 1500);
+    }, 1800);
 }
